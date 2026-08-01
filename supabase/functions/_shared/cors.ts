5ef00555e2f3f4a0f0c1f41e836230a72d5a4743
victor <victor@gmail.com>
2026-08-01 15:35:23 -0300

    Refatoração do projeto03

--- supabase/functions/_shared/cors.ts
+++
@@ -1,5 +0,0 @@
-// supabase/functions/_shared/cors.ts
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*', // Permite qualquer origem (para desenvolvimento)
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-}
